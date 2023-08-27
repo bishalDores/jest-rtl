8fbfd7b3f2fdab3118e70d8fa039074b0973e833
bishal <bishalpd@gmail.com>
2023-08-27 21:31:34 +0600

    testing color button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
+export const replaceCameWithSpaces = (colorName) => {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+};
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const [disabled, setDisabled] = useState(false);
+  const newBtnColor = buttonColor === "red" ? "blue" : "red";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button style={{ backgroundColor: disabled ? "gray" : buttonColor }} onClick={() => setButtonColor(newBtnColor)} disabled={disabled}>
+        Change to {newBtnColor}
+      </button>
+      <input type="checkbox" defaultChecked={disabled} onChange={(e) => setDisabled(e.target.checked)} id="disable-btn-checkbox" />
+      <label htmlFor="disable-btn-checkbox">Disable button</label>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,74 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+import { replaceCameWithSpaces } from "./App";
 
-test('renders learn react link', () => {
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // find an element with a role of button and text of Change to blue
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton).toHaveTextContent("Change to red");
+});
+
+test("initial conditions", () => {
+  render(<App />);
+
+  // btn enable initially
+  const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorBtn).toBeEnabled();
+
+  // check that checkbox starts out unchecked
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
+});
+
+test("Checkbox disables button on first click and enables on second click", () => {
+  render(<App />);
+
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+  const colorBtn = screen.getByRole("button");
+
+  fireEvent.click(checkbox);
+  expect(colorBtn).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(colorBtn).toBeEnabled();
+});
+
+test("Button is gray when it is disabled and red when it is enabled", () => {
+  render(<App />);
+  const checkbox = screen.getByRole("checkbox", { name: "Disable button" });
+  const colorBtn = screen.getByRole("button");
+
+  // expect the background color to be gray when disabled
+  fireEvent.click(checkbox);
+  expect(colorBtn).toBeDisabled();
+  expect(colorBtn).toHaveStyle({ backgroundColor: "gray" });
+
+  // expect the background color to be red when enabled
+  fireEvent.click(checkbox);
+  expect(colorBtn).toBeEnabled();
+  expect(colorBtn).toHaveStyle({ backgroundColor: "red" });
+});
+
+describe("spaces before camel-case capital letters", () => {
+  test("Works for no inner capital letters", () => {
+    expect(replaceCameWithSpaces("Red")).toBe("Red");
+  });
+  test("Works for one inner capital letter", () => {
+    expect(replaceCameWithSpaces("MidnightBlue")).toBe("Midnight Blue");
+  });
+  test("Works for multiple inner capital letters", () => {
+    expect(replaceCameWithSpaces("MediumVioletRed")).toBe("Medium Violet Red");
+  });
 });
